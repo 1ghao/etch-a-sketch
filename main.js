51e1e1ee99fd0976728168997a5ec46a171402e5
1ghao <linwanghao2007@gmail.com>
2024-03-14 22:42:48 +0100

    create createGrid function which creates a grid of given side

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 //create 16 divs
 const gridContainer = document.querySelector(".gridContainer");
-for (let i = 0; i < 16; i++) {
-  let genericDiv = document.createElement("div");
-  gridContainer.appendChild(genericDiv);
-}
+createGrid(4);
 
 gridContainer.addEventListener("mouseover", (e) => {
   let target = e.target;
   target.style.backgroundColor = "gray";
 });
 
-const gridCreator = document.querySelector("button");
+const creatorButton = document.querySelector("button");
 
-gridCreator.addEventListener("click", () => {
+creatorButton.addEventListener("click", () => {
   let answer;
   do {
     answer = parseInt(prompt("What is the length of the desired side?"));
   } while (answer > 10 || answer <= 0 || isNaN(answer));
 
-  answer;
+  createGrid(answer);
+  let gridChilds = Array.from(gridContainer.childNodes);
+  gridChilds.map((child) => {
+    child.style.flexBasis = `${Math.floor(100 / answer)}%`;
+  });
 });
 
 function createGrid(sideLength) {
