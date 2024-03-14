69f6f26514b75eba1c8f8711e5161c2a7f8b0adc
1ghao <linwanghao2007@gmail.com>
2024-03-14 22:29:20 +0100

    square divs in the grid and center button

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ gridContainer.addEventListener("mouseover", (e) => {
   let target = e.target;
   target.style.backgroundColor = "gray";
 });
+
+const gridCreator = document.querySelector("button");
+
+gridCreator.addEventListener("click", () => {
+  let answer;
+  do {
+    answer = parseInt(prompt("What is the length of the desired side?"));
+  } while (answer > 10 || answer <= 0 || isNaN(answer));
+
+  answer;
+});
+
+function createGrid(sideLength) {
+  gridContainer.textContent = "";
+  for (let i = 0; i < sideLength ** 2; i++) {
+    let genericDiv = document.createElement("div");
+    gridContainer.appendChild(genericDiv);
+  }
+}
